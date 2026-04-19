f2d9dd501c1174417d5a91bea147b7d32c0f1b41
justKirwl <honeyrot222@gmail.com>
2026-04-19 18:38:36 +0300

    Library version: added 1/2 versions

---
+++ python-nmap.py
@@ -0,0 +1,29 @@
+import nmap
+from typing import Iterable
+
+nm = nmap.PortScanner()
+
+def scan_ports(ports: Iterable[int] = [], host: str = 'localhost', colored_output: bool = True) -> None:
+    BLUE = '\033[94m' if colored_output else ''
+    GREEN = '\033[92m' if colored_output else ''
+    BLACK_ON_BLUE = '\033[34m' if colored_output else ''
+    RESET = '\033[0m' if colored_output else ''
+
+    nm.scan(host, ",".join([str(port) for port in ports]) if ports else None)
+
+    for host in nm.all_hosts():
+        print('-' * 30)
+        print(f"Host: {BLUE}{host}{RESET}")
+        print(f"State: {nm[host].state()}")
+
+        for proto in nm[host].all_protocols():
+            print('-' * 15)
+            print(f'Protocol: {proto}')
+
+            lport: list[int] = list(nm[host][proto].keys())
+            lport.sort()
+            for port in lport:
+                state = nm[host][proto][port]['state']
+                print(f'port : {GREEN}{port}{RESET}  state: {BLACK_ON_BLUE}{state}{RESET}')
+
+scan_ports()
